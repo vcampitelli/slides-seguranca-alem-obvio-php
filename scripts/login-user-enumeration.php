a16f6db9542eed6014ed4da4324921cdd0054384
Vinícius Campitelli <1877191+vcampitelli@users.noreply.github.com>
2019-06-06 16:32:00 -0300

    Adicionando vídeo de demonstração dos scripts de user enumeration

--- scripts/login-user-enumeration.php
+++ scripts/login-user-enumeration.php
@@ -3,7 +3,7 @@
  * Script para testar enumeração de usuários através de timing attacks
  *
  * Uso: php login-user-enumeration.php user pass
- * Benchmark: time for i in $(seq 1 100); do php login-user-enumeration.php user pass; done
+ * Benchmark: time for i in {1..100}; do php login-user-enumeration.php user pass; done
  *
  * @author Vinícius Campitelli
  */
@@ -17,8 +17,7 @@ class UserLogin
      * @var array
      */
     private $users = [
-        'user1' => '$argon2i$v=19$m=1024,t=2,p=2$S0p6RE5MY0RmZDQuZS43cA$tQW1qre4LBVk8hIbnAt+7hr3wORe3RDx9pvDDwG5FxY', // "pass1"
-        'user2' => '$argon2i$v=19$m=1024,t=2,p=2$UHR0RmtWbUxTQy5JT2tGRQ$xlI5gx8ak+DGWCgo2uFRg4SlInP6DFBNi1yc0UWGteg', // "pass2"
+        'validuser' => '$argon2i$v=19$m=1024,t=2,p=2$aW1rNXpraFdLWjI5Q1JMag$lChRF0TVyzaT0wC8INlJF1rNTChsKFEuICa9e/gli6c', // "validpass"
     ];
 
     public function login(string $username, string $password) : bool
@@ -47,19 +46,19 @@ class UserLogin
 
 // Argumento usuário
 if (! isset($argv[1])) {
-    echo 'Usuário não especificado' . PHP_EOL;
+    echo "\e[31m0\e[0m";
     die(1);
 }
 
 // Argumento senha
 if (! isset($argv[2])) {
-    echo 'Senha não especificada' . PHP_EOL;
+    echo "\e[31m0\e[0m";
     die(1);
 }
 
 if (! (new UserLogin)->login($argv[1], $argv[2])) {
-    echo 'Usuário/senha incorretos' . PHP_EOL;
+    echo "\e[31m0\e[0m";
     die(2);
 }
 
-echo 'Logado!' . PHP_EOL;
+echo "\e[32m1\e[0m";
